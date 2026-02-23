d49863226772ca3cca0d86e50e589f9923c47243
shirurona <kawakana0510@gmail.com>
2026-02-23 18:35:06 +0000

    fix asset path

--- frontend/worker/index.ts
+++ frontend/worker/index.ts
@@ -1,5 +1,5 @@
 export default {
-  fetch(request) {
+  fetch(request, env) {
     const url = new URL(request.url);
 
     // /api/* へのリクエストを Go バックエンドに転送
@@ -7,6 +7,13 @@ export default {
       const backendPath = url.pathname.replace("/api", "");
       return fetch(`http://localhost:1323${backendPath}`);
     }
+
+    // /bingo/* へのリクエストは Cloudflare のアセットを返す
+    if (url.pathname.startsWith("/bingo")) {
+      url.pathname = url.pathname.replace("/bingo", "");
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
     return new Response(null, { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
